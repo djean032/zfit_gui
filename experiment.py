f0105aed0dc76a76eb29fe184b8e2969d8ad6c7c
Dairen Jean <59145040+djean032@users.noreply.github.com>
2025-09-02 16:42:30 -0400

    initial

---
+++ experiment.py
@@ -0,0 +1,19 @@
+import nidaqmx
+import numpy as np
+import numpy.typing as npt
+
+from nidaqmx.constants import AcquisitionType, READ_ALL_AVAILABLE
+
+class zscan:
+
+    def __init__(self, zlim: float, zsamp: int) -> None:
+        self.ready_to_measure = False
+        self.measurement_complete = False
+        self.zlim = zlim
+        self.zsamp = zsamp
+        self.zpos = np.linspace(-self.zlim, self.zlim, zsamp)
+
+
+    def collect(self) -> npt.NDArray:
+        measurements = np.zeros(shape=(2, self.zsamp))
+        return measurements
