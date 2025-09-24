75afca60efc5b8762d9b7340827095fdba67b5b4
djean032 <djean032@gmail.com>
2025-09-23 21:32:10 -0400

    fixed type hinting issues with jax arrays

--- experiment.py
+++ experiment.py
@@ -4,7 +4,7 @@ import jax
 import jax.numpy as jnp
 import nidaqmx
 import numpy as onp
-from jax.typing import ArrayLike
+from jaxtyping import Array
 
 from esp302 import ESP302
 
@@ -13,15 +13,15 @@ jax.config.update("jax_enable_x64", True)
 
 class Experiment:
     def __init__(self, zlim: float, zsamp: int, samp_per_pos: int = 4) -> None:
-        self.zlim = zlim
-        self.zsamp = zsamp
-        self.zpos = jnp.linspace(-self.zlim, self.zlim, zsamp)
-        self.samp_per_pos = samp_per_pos
-        self.stage = ESP302()
-        self.zaxis = 3
+        self.zlim: float = zlim
+        self.zsamp: int = zsamp
+        self.zpos: Array = jnp.linspace(-self.zlim, self.zlim, zsamp)
+        self.samp_per_pos: int = samp_per_pos
+        self.stage: ESP302 = ESP302()
+        self.zaxis: int = 3
 
-    def collect(self) -> ArrayLike:
-        measurements = jnp.zeros(shape=(self.zsamp, 3))
+    def collect(self) -> Array:
+        measurements: Array = jnp.zeros(shape=(self.zsamp, 3))
         with nidaqmx.Task() as task:
             task.ai_channels.add_ai_voltage_chan(
                 "Dev1/ai0", min_val=-10.0, max_val=10.0
@@ -32,7 +32,10 @@ class Experiment:
             for index, position in enumerate(self.zpos):
                 self.step(position)
                 try:
-                    tmp_val = task.read(number_of_samples_per_channel=self.samp_per_pos)
+                    # Type hinting for this is a nightmare, about a dozen OR statements. Should always be a list in this case...
+                    tmp_val = task.read(
+                        number_of_samples_per_channel=self.samp_per_pos  # type: ignore
+                    )
                     # [TODO] insert some validation code to verify tmp_val list is in window
                     measurements.at[index, :].set(
                         [
@@ -56,7 +59,7 @@ class Experiment:
 
         return 0
 
-    def save_all_data(self, measurement_data: ArrayLike, name: str):
+    def save_all_data(self, measurement_data: Array, name: str):
         onp.savetxt(
             name + ".csv",
             measurement_data,
@@ -64,13 +67,13 @@ class Experiment:
             header="Position (mm),ai0,ai1",
         )
 
-    def save_norm_data(self, measurement_data: ArrayLike, name: str):
+    def save_norm_data(self, measurement_data: Array, name: str):
         norm_data = self.normalize(measurement_data)
         onp.savetxt(
             name + ".csv", norm_data, delimiter=",", header="Position (mm),Transmission"
         )
 
-    def normalize(self, measurement_data: ArrayLike) -> ArrayLike:
+    def normalize(self, measurement_data: Array) -> Array:
         transmission = jnp.zeros(self.zsamp)
         transmission = measurement_data[:, 1] / measurement_data[:, 2]
         norm_factor = jnp.mean(transmission[0:10])
